5b176258dc81a6563884760912e8935fbdb5df4d
Emmanuel-TSP <lopez.paredes.emmanuel@gmail.com>
2024-06-15 21:56:20 -0600

    Cambios Finales

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <locale>
+#include <algorithm>
 #include "movies.hpp"
 #include "series.hpp"
 using namespace std;
@@ -11,6 +12,22 @@ int main() {
     int opcion;
     bool repeat = true;
     int rating; // Declare rating variable outside switch
+
+    // Declared variables for cases 5 and 6
+    string rate_user = "";
+    string ID_num = "";
+    string type_video = "";
+    string episode_user = "";
+    string ID_series = "";
+
+    vector<string> IDseries_options= {"S1","S2","S3","S4","S5","S6","S7","S8","S9","S10"}; 
+
+    //Possible number ID's
+    vector<string> ID_options= {"1","2","3","4","5","6","7","8","9","10"}; 
+    //Possible rating numbers
+    vector<string> rate_options= {"1","2","3","4","5"}; 
+    //Possible episode numbers
+    vector<string> ep_options= {"1","2","3","4","5"}; 
 
     Movies movie;
     movie.loadMovies("movies.csv");
@@ -27,7 +44,7 @@ int main() {
         cout << "Menu" << endl;
         cout << "  1. Show all videos" << endl;
         cout << "  2. Show videos with a specific rating or specific genre." << endl;
-        cout << "  3. Show videos of a series by its rating." << endl;
+        cout << "  3. Show videos of a specific series with specific rating." << endl;
         cout << "  4. Show movies by their rating." << endl;
         cout << "  5. Rate a movie or series." << endl;
         cout << "  6. Exit" << endl;
@@ -46,7 +63,7 @@ int main() {
                 system("pause>nul"); 
                 break;
 
-            case 2:                /*No me muestra las pelis ni series*/
+            case 2:
                 system("cls");
                 cout << "Select an option: " << endl;
                 cout << "  By rating  (R)" << endl;
@@ -63,8 +80,10 @@ int main() {
                         if (rating >= 1 && rating <= 5)
                         {
                             cout << "Videos with rating " << rating << ": " << endl;
-                            cout << serie.showRatedSeries(ratingString) << endl;
+                            cout << "Movies with rating " << rating << ": " << endl;
                             cout << movie.showRatedMovies(ratingString) << endl;
+                            cout << "Series with rating " << rating << ": " << endl;
+                            cout << serie.showRatedSeries(ratingString) << endl;
                             system("pause>nul"); 
                             break;
                         }
@@ -84,36 +103,48 @@ int main() {
                         cin >> gen_choice;
                         if (gen_choice == "D")
                         {
+                            genre = "Drama";
                             cout << "Videos of drama genre: "<< endl;
-                            cout << serie.showGenreSeries("Drama") << endl;
-                            cout << movie.showGenreMovies("Drama") << endl;
+                            cout << "Movies of drama genre: "<< endl;
+                            cout << movie.showGenreMovies(genre) << endl;
+                            cout << "Series of drama genre: "<< endl;
+                            cout << serie.showGenreSeries(genre) << endl;
                             system("pause>nul"); 
                             break;
                         }
 
                         else if (gen_choice == "A")
                         {
+                            genre = "Action";
                             cout << "Videos of action genre: "<< endl;
-                            cout << serie.showGenreSeries("Action") << endl;
-                            cout << movie.showGenreMovies("Action") << endl;
+                            cout << "Movies of action genre: "<< endl;
+                            cout << movie.showGenreMovies(genre) << endl;
+                            cout << "Series of action genre: "<< endl;
+                            cout << serie.showGenreSeries(genre) << endl;
                             system("pause>nul"); 
                             break;
                         }
 
                         else if (gen_choice == "M")
                         {
+                            genre = "Mystery";
                             cout << "Videos of mystery genre: "<< endl;
-                            cout << serie.showGenreSeries("Mystery") << endl;
-                            cout << movie.showGenreMovies("Mystery") << endl;
+                            cout << "Movies of mystery genre: "<< endl;
+                            cout << movie.showGenreMovies(genre) << endl;
+                            cout << "Series of mystery genre: "<< endl;
+                            cout << serie.showGenreSeries(genre) << endl;
                             system("pause>nul"); 
                             break;
                         }
                         
                         else if (gen_choice == "S")
                         {
+                            genre = "Scifi";
                             cout << "Videos of scifi genre: "<< endl;
-                            cout << serie.showGenreSeries("Scifi") << endl;
-                            cout << movie.showGenreMovies("Scifi") << endl;
+                            cout << "Movies of scifi genre: "<< endl;
+                            cout << movie.showGenreMovies(genre) << endl;
+                            cout << "Series of scifi genre: "<< endl;
+                            cout << serie.showGenreSeries(genre) << endl;
                             system("pause>nul"); 
                             break;
                         }
@@ -121,6 +152,8 @@ int main() {
                         else
                         {
                             cout << "Invalid genre!"<< endl;
+                            system("pause>nul"); 
+                            break;
                         }
 
                     }
@@ -131,19 +164,30 @@ int main() {
             case 3: {
                                 system("cls");
 
-                cout << "Select a rating of series for us to search: " << endl;
-                cout << "From: 1-5" << endl;
-                cin >> rating; 
-                string ratingSerie = to_string(rating); 
+                cout << "Type the ID (letter + number) of a series which you want to see: " << endl;
+                cin >> ID_series;
+                if(find(IDseries_options.begin(), IDseries_options.end(), ID_series) != IDseries_options.end())
+                {
+                    cout << "Select a rating of series for us to search: " << endl;
+                    cout << "From: 1-5" << endl;
+                    cin >> rating; 
+                    string ratingSerie = to_string(rating); 
 
-                if (rating >= 1 && rating <= 5) {
-                    cout << "Series with rating: " << rating << ": " << endl;
-                    cout << serie.showRatedSeries(ratingSerie) << endl;
-                } else {
-                    cout << "Invalid rating!" << endl;
-                    cout << "Enter a number from 1 to 5 ." << endl;
+                    if (rating >= 1 && rating <= 5) {
+                        cout << "Videos of that series with rating: " << rating << ": " << endl;
+                        cout << serie.showRatedVideosSpecSeries(ID_series,ratingSerie) << endl;
+                    } else {
+                        cout << "Invalid rating!" << endl;
+                        cout << "Enter a number from 1 to 5 ." << endl;
+                    }
+
                 }
 
+                else
+                {
+                    cout << "Invalid ID. Try again!" << endl;
+
+                }
                 system("pause>nul");
                 break;
             }
@@ -168,9 +212,80 @@ int main() {
             }
 
             case 5:
+                system("cls");
+                cout << "Select the type of video you want to rate" << endl;
+                cout << "  Movie (M)" << endl;
+                cout << "  Serie (S)" << endl;
+                cin >> type_video;
+                if (type_video == "M")
+                {
+                    cout << "Type only the number ID of the movie you want" << endl;
+                    cin >> ID_num;
+                    if (find(ID_options.begin(), ID_options.end(), ID_num) != ID_options.end())
+                    {
+                        cout << "Select a number bewteen 1 and 5 to rate the movie" << endl;
+                        cin >> rate_user;
+                        if (find(rate_options.begin(), rate_options.end(), rate_user) != rate_options.end())
+                        {
+                            movie.rateMovie(ID_num,rate_user);
+                        }
+                        else
+                        {
+                            cout << "Incorrect rate. Try Again!" << endl;
+
+                        }
+                    }
+
+                    else
+                    {
+                        cout << "Incorrect number ID. Try Again!" << endl;
+                    }
+
+
+                }
+                else if (type_video == "S")
+                {
+                    cout << "Type only the number ID of the series you want" << endl;
+                    cin >> ID_num;
+                    if (find(ID_options.begin(), ID_options.end(), ID_num) != ID_options.end())
+                    {
+                        cout << "Type the episode number of that series" << endl;
+                        cin >> episode_user;
+                        if (find(ep_options.begin(), ep_options.end(), episode_user) != ep_options.end())
+                        {
+                            cout << "Select a number bewteen 1 and 5 to rate the movie" << endl;
+                            cin >> rate_user;
+                            if(find(rate_options.begin(), rate_options.end(), rate_user) != rate_options.end())
+                            {
+                                serie.rateSeries(ID_num,episode_user,rate_user);
+                            }
+                            else
+                            {
+                                cout << "Incorrect rate. Try Again!" << endl;
+                            }
+                        }
+                        else
+                        {
+                            cout << "Incorrect number of episode. Try Again!" << endl;
+                        }
+                    }
+
+                    else
+                    {
+                        cout << "Incorrect number ID. Try Again!" << endl;
+                    }
+                }
+                else
+                {
+                    cout << "Incorrect type of video. Try again!" << endl;
+                }
                 system("pause>nul"); 
                 break;
 
+
+                
+                
+                
             case 6:
                 repeat = false;
                 break;
@@ -186,27 +301,3 @@ int main() {
 
     return 0;
 }
-
-
-
-    /*
-    Movies movies;
-    movies.loadMovies("movies.csv");
-    cout << movies.ratedMovies();
-
-    Series serie;
-    serie.loadSeries("series.csv");
-    cout << serie.show();
-    cout << serie.showRatedSeries();
-    return 0;
-    */
-
-
-    //std::vector<Series> series = Series::loadSeries("series.csv");
-
-
-    //for (const auto& serie : series) {
-        //serie.show();
-    //}
-
-
